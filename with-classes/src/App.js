5aa0d8cc4379c0af7967de414caf398cb36bfa51
Katerina <katerinalupacheva@gmail.com>
2020-02-24 15:53:32 +0300

    VideoContextProvider and state moved to VideoContext

--- with-classes/src/App.js
+++ with-classes/src/App.js
@@ -1,46 +1,17 @@
 import React from 'react';
 import VideoClip from './components/video-clip.component';
 import Controls from './components/controls.component';
-import VideoContext from './context/video.context';
+import { VideoContextProvider } from './context/video.context';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.playVideo = () => {
-      let { status } = this.state;
-      if (status === 'playing') {
-        this.state.vidRef.current.play();
-      } else if (status === 'paused') {
-        this.state.vidRef.current.pause();
-      }
-    };
-
-    this.togglePlayPause = () => {
-      this.setState(
-        state => ({
-          vidRef: React.createRef(),
-          status: state.status === 'playing' ? 'paused' : 'playing',
-        }),
-        () => this.playVideo()
-      );
-    };
-
-    this.state = {
-      status: 'paused',
-      togglePlayPause: this.togglePlayPause,
-      vidRef: undefined,
-    };
-  }
-
   render() {
     return (
       <div className="App">
-        <VideoContext.Provider value={this.state}>
+        <VideoContextProvider>
           <VideoClip />
           <Controls />
-        </VideoContext.Provider>
+        </VideoContextProvider>
       </div>
     );
   }
--- with-classes/src/context/video.context.js
+++ with-classes/src/context/video.context.js
@@ -1,9 +1,36 @@
-import { createContext } from 'react';
+import React, { createContext } from 'react';
 
-const VideoContext = createContext({
-  status: 'paused',
-  togglePlayPause: () => {},
-  vidRef: undefined,
-});
+const VideoContext = createContext({});
+
+export class VideoContextProvider extends React.Component {
+  playVideo = () => {
+    let { status } = this.state;
+    if (status === 'playing') {
+      this.state.vidRef.current.play();
+    } else if (status === 'paused') {
+      this.state.vidRef.current.pause();
+    }
+  };
+
+  togglePlayPause = () => {
+    this.setState(
+      state => ({
+        ...state,
+        status: state.status === 'playing' ? 'paused' : 'playing',
+      }),
+      () => this.playVideo()
+    );
+  };
+
+  state = {
+    status: 'paused',
+    togglePlayPause: this.togglePlayPause,
+    vidRef: React.createRef(),
+  };
+
+  render() {
+    return <VideoContext.Provider value={this.state}>{this.props.children}</VideoContext.Provider>;
+  }
+}
 
 export default VideoContext;
